acfa9ea77ecdc30a8f978069b5e94453599229cf
maratmarat22 <akhmyarovmarat@mail.ru>
2026-02-11 11:36:03 +0300

    Добавить определенные вещи 3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'auto-mini-app-v2-refactored',
+  base: '/auto-mini-app-v2-refactored/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
